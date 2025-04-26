9a5e2d613cd1776070b0f0fee71f30d7272c4ef5
Isaiah <schultz.isaiah@gmail.com>
2025-04-26 10:41:19 -0500

    GH report

--- src/main/kotlin/com/heliopolis/p3x972/spock/springboot/demo/domain/Stuff.kt
+++
@@ -1,7 +0,0 @@
-package com.heliopolis.p3x972.spock.springboot.demo.domain
-
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonInclude.Include
-
-@JsonInclude(Include.NON_EMPTY)
-data class Stuff(var value: String)
--- src/main/kotlin/com/heliopolis/p3x972/spock/springboot/demo/domain/ThingAndStuff.kt
+++
@@ -1,7 +0,0 @@
-package com.heliopolis.p3x972.spock.springboot.demo.domain
-
-import com.heliopolis.p3x972.spock.springboot.demo.entity.Thing
-
-
-class ThingAndStuff(thing: Thing, private val listOfStuff: List<Stuff>) :
-    BaseThing(thing.id, thing.name, thing.updateDate)
